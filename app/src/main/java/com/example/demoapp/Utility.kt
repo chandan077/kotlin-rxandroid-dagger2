db0cfc423779f6e8b71809309f17074d19fe36a9
Chandan Kumar <chandan.kumar2@go-mmt.com>
2020-05-17 23:32:00 +0530

    Kotlin-Dagger2-RxAndroid-MVVM Example

--- app/src/main/java/com/example/demoapp/Utility.kt
+++ app/src/main/java/com/example/demoapp/Utility.kt
@@ -2,10 +2,6 @@ package com.example.demoapp
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 
